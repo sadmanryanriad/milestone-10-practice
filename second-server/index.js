676c924cdb2a7a1d1da026f6742e8ba51c52fae4
sadmanryanriad <sadmanryanriad@gmail.com>
2023-10-17 15:26:22 +0600

    dotenv added

--- second-server/index.js
+++ second-server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { ObjectId } = require('mongodb');
 const cors = require('cors');
+require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,12 +10,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//coffee
-//MA57VivpFWvdEjZi
-
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://coffee:MA57VivpFWvdEjZi@cluster0.ni8nft9.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ni8nft9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
